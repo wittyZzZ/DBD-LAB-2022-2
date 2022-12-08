71c977e0e5e7bcd9681a5cadf5e84af59406622b
vicenterojash <vicente.rojas.h@usach.cl>
2022-12-08 20:06:00 -0300

    mensaje

---
+++ demoxd/src/main/java/com/dbdgrupo16/demo/models/Rol.java
@@ -0,0 +1,41 @@
+package com.dbdgrupo16.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="rol")
+
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id_rol")
+    private Integer id_rol;
+
+    @Column(name = "nombre_rol")
+    private String nombre_rol;
+
+    public Rol(){
+    }
+
+    //Constructor
+    public Rol(Integer id_rol,String nombre_rol){
+        this.id_rol = id_rol;
+        this.nombre_rol = nombre_rol;
+    }
+
+    //Getters
+    public Integer getId_rol(){ return id_rol;}
+    public String getNombre_rol(){ return nombre_rol;}
+
+    //Setters
+    public void setId_rol(Integer id_rol){ this.id_rol = id_rol;}
+    public void setNombre_rol(String nombre_rol){ this.nombre_rol = nombre_rol;}
+
+    @Override
+    public String toString(){
+        return "Rol [id_rol=]"+id_rol +
+                ", nombre_rol= '"+ nombre_rol+
+                ']';
+    }
+
+}
